987745f34086584925a14cb6587bef7e47646cf2
Nikola M <nikolamilovic2001@gmail.com>
2022-10-21 18:08:36 +0200

    remove even more files

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,7 +1,3 @@
-import config from "./config";
-import Logger from "./loaders/logger";
-import startupSetup from "./loaders/";
-
 import "reflect-metadata"; // We need this in order to use @Decorators
 import express from "express";
 import { Server } from "http"
@@ -12,16 +8,9 @@ let app: express.Application;
 async function startServer() {
     app = express();
 
-    await startupSetup({ expressApp: app });
-
     server = app
-        .listen(config.port, () => {
-            Logger.debug(
-                `Server listening on port: ${config.port} at ${config.environment} mode`
-            );
-        })
+        .listen(3000, () => {})
         .on("error", (err) => {
-            Logger.error(err);
             process.exit(1);
         });
 }
